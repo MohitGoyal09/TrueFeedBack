678364c3fed88adff1c584dee65614e6c20c626d
MohitGoyal09 <goel0632@gmail.com>
2024-12-07 14:53:41 +0530

    Update sender email format in sendVerificationEmail function

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: "dev@mohitg.xyz",
+      from: "Acme <noreply@mohitg.xyz>",
       to: email,
       subject: "Mystery Message Verification Code",
       react: VerificationEmail({ username, otp: verifyCode }),
